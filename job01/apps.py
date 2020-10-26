d6bd0949f3af1253ced6ab42b0713141ebbf4b8c
Junhee Cho <junhee3085@gmail.com>
2020-10-26 16:40:18 +0900

    add job01 app

---
+++ job01/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Job01Config(AppConfig):
+    name = 'job01'
---
+++ job01/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def list_job01(request):
+    pass
