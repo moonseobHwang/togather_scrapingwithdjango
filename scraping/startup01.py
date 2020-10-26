163481b01a8fa03935ef9cde2df52c7dd2d5cb0b
SanghunOh <otter.oh@gmail.com>
2020-10-26 15:17:46 +0900

    first commit

---
+++ scraping/startup01.py
@@ -0,0 +1,17 @@
+import schedule
+import time
+
+def job():
+    print("I'm working...")
+
+schedule.every(5).seconds.do(job)
+
+# reference other file
+import sample_function
+schedule.every(3).seconds.do(sample_function.print_message)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+# run python3 ./startup02.py    # in Terminal
