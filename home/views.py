4ca4fb333128d52c89bf617c95f378c9c788e759
sayitinmyface <leo99roo@gmail.com>
2020-10-26 15:48:40 +0900

    10261548/sayit

--- home/views.py
+++ home/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
+from 
 # Create your views here.
 from django.shortcuts import render		# add
 def home(request):
     return render(request, 'home/home.html')
+
+def startup(req):
+    db_url = 'mongodb://192.168.0.171:27017'
+    data = {}
+    with MongoClient(db_url) as clien:
+        mydb = clien.mydb
+        result = list(mydb.startup.find({}))
+    # 
+    page = req.GET.get('page',1)
+    result_page = Paginator(result,10)
+    data['page_obj'] = result_page.get_page(page)    
+    # 
+    return render(request, 'home/startup.html',context=data)
